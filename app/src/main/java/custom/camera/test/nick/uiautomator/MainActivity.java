e394bcd7a9fb1ec7dcedf6a7cbacb3c5f18b50c7
nicolalandro <nicolaxx94@live.it>
2017-07-28 11:38:13 +0200

    WIP: implementata list view, ma senza test

--- app/src/main/java/custom/camera/test/nick/uiautomator/MainActivity.java
+++ app/src/main/java/custom/camera/test/nick/uiautomator/MainActivity.java
@@ -3,9 +3,13 @@ package custom.camera.test.nick.uiautomator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -13,6 +17,10 @@ public class MainActivity extends AppCompatActivity
     private TextView mTextView;
     private EditText mEditText;
     private CheckBox mCheckBox;
+
+    private ListView mListView;
+    ArrayList<String> mListItems = new ArrayList<>();
+    ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,15 +31,19 @@ public class MainActivity extends AppCompatActivity
         mTextView = (TextView) findViewById(R.id.text_view);
         mEditText = (EditText) findViewById(R.id.edit_text);
         mCheckBox = (CheckBox) findViewById(R.id.checkBox);
+
+        mListView = (ListView) findViewById(R.id.list_view);
+        mAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                mListItems);
+        mListView.setAdapter(mAdapter);
     }
 
-    public void click(View v) {
-        if(mCheckBox.isChecked()){
-            mTextView.setText(mEditText.getText().toString().toUpperCase());
-        }
-        else
-        {
-            mTextView.setText(mEditText.getText());
-        }
+    public void click(View v)
+    {
+        String s = mCheckBox.isChecked() ? mEditText.getText().toString().toUpperCase() : mEditText.getText().toString();
+        mTextView.setText(s);
+        mAdapter.add(s);
+
     }
 }
